e1462097eb36d995d4c39d864ada6360563099f8
ivary43 <contactmanish16@gmail.com>
2017-11-02 22:15:27 +0530

    added payment page

---
+++ app/src/main/java/com/anweshainfo/anwesha_registration/payment_activity.java
@@ -0,0 +1,8 @@
+package com.anweshainfo.anwesha_registration;
+
+/**
+ * Created by manish on 2/11/17.
+ */
+
+public class payment_activity {
+}
---
+++ app/src/main/java/com/anweshainfo/anwesha_registration/reg_result.java
@@ -0,0 +1,13 @@
+package com.anweshainfo.anwesha_registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reg_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_result);
+    }
+}
